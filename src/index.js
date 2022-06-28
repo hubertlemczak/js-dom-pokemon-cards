2b883255621584689031faf731d4a0b97f3ff2ff
Hubert Lemczak <hubertlemczak@gmail.com>
2022-06-28 13:43:39 +0100

    complete

--- src/index.js
+++ src/index.js
@@ -84,25 +84,4 @@ const createCard = (pokemon) => {
 
 data.forEach((pokemon) => {
   createCard(pokemon);
-  // const card = `
-  // <li class="card" onclick="changeImg('card--img${num}','${images[0]}','${images[1]}','${images[2]}')">
-  //   <h2 class="card--title">${pokemonName}</h2>
-  //   <img
-  //   width="256"
-  //   class="card--img card--img${num}"
-  //   src=${pokemon.sprites.other['official-artwork'].front_default}
-  //   />
-  //   <ul class="card--text">
-  //     <li>HP: ${pokemon.stats[0].base_stat}</li>
-  //     <li>ATTACK: ${pokemon.stats[1].base_stat}</li>
-  //     <li>DEFENSE: ${pokemon.stats[2].base_stat}</li>
-  //     <li>SPECIAL-ATTACK: ${pokemon.stats[3].base_stat}</li>
-  //     <li>SPECIAL-DEFENSE: ${pokemon.stats[4].base_stat}</li>
-  //     <li>SPEED: ${pokemon.stats[5].base_stat}</li>
-  //     <li class="wrap-words" style="max-width: 350px;">Games: ${games}</li>
-  //   </ul>
-  // </li>
-  // `;
-  // num++;
-  // cards.innerHTML += card;
 });
